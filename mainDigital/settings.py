3f1b29dabc761ac202a6c0d3b66c8be030217b00
Ankit Bhattarai <ablive817@gmail.com>
2024-09-25 15:05:37 +0545

    admin lte removed

--- mainDigital/settings.py
+++ mainDigital/settings.py
@@ -36,8 +36,6 @@ ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 # Application definition
 
 INSTALLED_APPS = [
-    'adminlte3',
-    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
