d64a491a98ab2a0812180739770b93e971f23268
Ankit Bhattarai <ablive817@gmail.com>
2024-09-25 17:04:26 +0545

    status updated

--- smsDigital/models.py
+++ smsDigital/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SMSLog(models.Model):
     mobile_number = models.CharField(max_length=15)
     message = models.TextField()
-    status = models.CharField(max_length=10)  # Sent, Failed, etc.
+    status = models.CharField(max_length=250)  # Sent, Failed, etc.
     sent_at = models.DateTimeField(auto_now_add=True)
     response = models.TextField(null=True, blank=True)
 
