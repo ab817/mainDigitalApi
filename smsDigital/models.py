5b13c6e54d0618fc7b7a9c930683bd6f74233430
Ankit Bhattarai <ablive817@gmail.com>
2024-10-03 13:46:11 +0545

    migration issue update

--- smsDigital/models.py
+++ smsDigital/models.py
@@ -1,5 +1,8 @@
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class SMSLog(models.Model):
     mobile_number = models.CharField(max_length=15)
